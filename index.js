cbf040a100217d9e492fa0a4202e0034320888c8
FarisPalayi <fariswritecode@gmail.com>
2021-09-03 00:07:58 +0530

    cli intro

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const { readdir, rename } = require("fs/promises");
 const path = require("path");
 const { createInterface } = require("readline");
+
+const rl = createInterface({ input: process.stdin, output: process.stdout}, err => new Error(err));
+
+process.stdout.write("\n Remove keywords from filename.\n")
+
+rl.question(`\nKeyword to remove from the filename? eg: min\n`,
+  keyword => {
+    if (keyword) {
+      removeFromFileName(keyword)
+      rl.close();
+    } else {
+      rl.write("Invalid input");
+    }
+  }
+)
