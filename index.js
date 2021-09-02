3c269f169249140c3e8657c8881ea5f33e4910db
FarisPalayi <fariswritecode@gmail.com>
2021-09-03 00:11:38 +0530

    File renames stuff successfully ✔

--- index.js
+++ index.js
@@ -2,21 +2,6 @@ const { readdir, rename } = require("fs/promises");
 const path = require("path");
 const { createInterface } = require("readline");
 
-const rl = createInterface({ input: process.stdin, output: process.stdout}, err => new Error(err));
-
-process.stdout.write("\n Remove keywords from filename.\n")
-
-rl.question(`\nKeyword to remove from the filename? eg: min\n`,
-  keyword => {
-    if (keyword) {
-      removeFromFileName(keyword)
-      rl.close();
-    } else {
-      rl.write("Invalid input");
-    }
-  }
-)
-
 async function _allFiles(folderPath) {
   return await readdir(folderPath, "utf-8")
     .then((file) => file)
@@ -34,3 +19,28 @@ async function _rename(folderPath, file, keyword) {
       .catch((err) => console.log("Error renaming files \n", err));
   }
 }
+
+async function removeFromFileName(keyword = "", folderPath = __dirname) {
+  const files = await _allFiles(folderPath);
+  const filesToRename = files.filter(file => file.includes(keyword));
+
+  if (!filesToRename.length)
+    return process.stdout.write(`No file matching the keyword '${keyword}'`);
+
+  filesToRename.forEach(file => _rename(folderPath, file, keyword));
+}
+
+const rl = createInterface({ input: process.stdin, output: process.stdout}, err => new Error(err));
+
+process.stdout.write("\n Remove keywords from filename.\n")
+
+rl.question(`\nKeyword to remove from the filename? eg: min\n`,
+  keyword => {
+    if (keyword) {
+      removeFromFileName(keyword)
+      rl.close();
+    } else {
+      rl.write("Invalid input");
+    }
+  }
+)
